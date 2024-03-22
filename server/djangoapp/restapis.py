70735cd010bb9b11c604c99bbc86209249cf2abe
Ahmet Ozer <cagriahmet32@gmail.com>
2024-03-22 17:12:34 -0400

    Dealer list and individual dealer view added

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -12,22 +12,33 @@ sentiment_analyzer_url = os.getenv(
     default="http://localhost:5050/")
 
 # def get_request(endpoint, **kwargs):
+import requests
+
 def get_request(endpoint, **kwargs):
-    params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+    params = "&".join([f"{key}={value}" for key, value in kwargs.items()]) if kwargs else ""
+    request_url = backend_url + endpoint + "?" + params
 
-    request_url = backend_url+endpoint+"?"+params
-
-    print("GET from {} ".format(request_url))
+    print("GET request to: {}".format(request_url))
+    
     try:
-        # Call get method of requests library with URL and parameters
+        # Make the GET request
         response = requests.get(request_url)
-        return response.json()
-    except:
-        # If any error occurs
-        print("Network exception occurred")
+        
+        # Print response status code and content
+        print("Response status code:", response.status_code)
+        print("Response content:", response.content)
+        
+        # Check if response is successful (status code 200)
+        if response.status_code == 200:
+            # Parse JSON response
+            return response.json()
+        else:
+            # If response is not successful, raise an exception
+            response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        # Handle any exceptions (e.g., network errors, invalid response)
+        print("Error occurred:", e)
+
 
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"analyze/"+text
